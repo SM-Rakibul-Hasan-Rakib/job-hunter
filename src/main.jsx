b701fc43996b9ab315fccc7ce13700386bbea2c8
SM-Rakibul-Hasan-Rakib <devrakibulhasanrakib@gmail.com>
2025-07-28 20:29:04 +0600

    1st commit

---
+++ src/main.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./Components/Root/Root";
+import Home from "./Components/Home/Home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
